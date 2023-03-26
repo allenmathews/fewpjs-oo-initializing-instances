1d417c3fcba7d1d3fdabcd9f79678142a933e719
Allen Mathews <allenjoemathews@gmail.com>
2023-03-27 10:13:09 +1100

    commit tree 6a88d1d83640cd157c963b66a1f9aaa5d7fa5cb3

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code here
+
+class Breakfast {
+    constructor(food, drink) {
+        this.food = food;
+        this.drink = drink;
+    }
+}
+
+let breakfast1 = new Breakfast('eggs')
+
+
+
+class Lunch {
+    constructor(salad, soup, drink) {
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+let lunch1 = new Lunch('sandwiches')
+
+class Dinner {
+    constructor(salad, soup, entree, _dessert) {
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this._dessert = _dessert;
+    }
+}
+let dinner1 = new Dinner('stir fry')
